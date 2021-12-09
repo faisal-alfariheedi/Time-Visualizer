30cd47cbdc5eabd89b0108f378e7d0b665f2245f
faisal alfariheedi <falfariheedi@gmail.com>
2021-12-09 07:54:49 +0300

    Initial Commit

---
+++ Time Visualizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Time Visualizer
+//
+//  Created by faisal on 05/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
